3c29b1d354a9c33cdfd0b68c82974fb5a33a71c7
hioleong <hiowengleong@gmail.com>
2015-01-18 20:53:40 +0000

    Initial Commit

---
+++ public/javascripts/fb_login.js
@@ -0,0 +1,58 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+    } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+    } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into Facebook.';
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+    FB.getAuthResponse(function(response) {
+        console.log(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+            appId      : '756388677788740',
+            xfbml      : true,
+            version    : 'v2.2'
+    });
+};
+
+// Insert FB SDK into JS
+(function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log(response);
+        console.log('Successful login for: ' + response.id);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
+
